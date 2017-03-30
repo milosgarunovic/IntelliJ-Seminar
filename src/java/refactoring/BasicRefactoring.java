f485600af62b2a50a01e081957158bc2a120a614
Miloš Garunović <milos.garunovic@gmail.com>
2017-03-31 00:01:10 +0200

    neka porukica

--- src/java/refactoring/BasicRefactoring.java
+++ src/java/refactoring/BasicRefactoring.java
@@ -4,17 +4,24 @@ import util.Person;
 
 public class BasicRefactoring {
 	
+	public static void main(String[] args) {
+
 //	F5
-	
+
 //	F6
-	
+
 //	Shift + F6
 //  -> To Employee
-	Person p = new Person("Milos");
-	
+		Person p = new Person("Milos");
+		Person p2 = new Person("Milos");
+		Person p3 = new Person("Milos");
+		Person p4 = new Person("Milos");
+
 //	Ctrl + Alt + V
-	
+
 //	Ctrl + Alt + M
-	
+		
+		
+	}
 	
 }
